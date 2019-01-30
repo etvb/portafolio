887a7fcd2737256cc1d3c6f5e454a22816f02831
Edgardo Torres <Edgardo.torres@ad.mediaccess.corp>
2019-01-29 20:35:00 -0600

    Menu terminado se puede mejorar

---
+++ js/ocultar.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+	$('.menu').click(function(){
+		$('ul').toggleClass('active');
+	})
+	// $('.MainMenu-link').click(function(){
+	// 	$('ul').toggleClass('unactive');
+	// })
+})
